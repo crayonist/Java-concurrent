d38194b770f4155490884bced6da71944451a511
crayonist <hzxcrayon@163.com>
2019-01-27 22:43:14 +0800

    add commit

--- src/main/java/com/labi/thread/communication/ProducerConsumer.java
+++ src/main/java/com/labi/thread/communication/ProducerConsumer.java
@@ -2,6 +2,9 @@ package com.labi.thread.communication;
 
 import java.util.concurrent.LinkedBlockingDeque;
 
+/**
+ * Simple Producer-Consumer Model
+ */
 public class ProducerConsumer {
 
     private static final int MAX_SIZE = 5;
@@ -14,8 +17,11 @@ public class ProducerConsumer {
             int i = 0;
             while (true) {
                 synchronized (queue) {
+                    // 消费队列满，则等待队列空闲
                     while (queue.size() == MAX_SIZE) {
                         try {
+                            // 挂起当前线程，并释放通过同步块获取的queue上的锁，让消费者线程可以获取该锁，然后获取队列里面的元素
+                            // 注意：当前线程调用共享变量的wait()后只会释放当前共享变量上的锁，如果当前线程还持有其他共享变量的锁，这些锁是不会被释放的。
                             queue.wait();
                         } catch (InterruptedException e) {
                             e.printStackTrace();
@@ -26,7 +32,7 @@ public class ProducerConsumer {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    queue.add("Produce");
+                    queue.add("Product");
                     System.out.println("The producer produced "+ i++ +" product.");
                     queue.notify();
                 }
@@ -48,11 +54,13 @@ public class ProducerConsumer {
                         }
                     }
                     try {
+                        // 消费队列元素
                         queue.take();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     System.out.println("The consumer consume "+ i++ +" product.");
+                    // 消费完通知唤醒生产者线程
                     queue.notify();
                 }
             }
@@ -60,12 +68,10 @@ public class ProducerConsumer {
     }
 
     public static void main(String[] args) {
-
         Producer p = new Producer();
         p.start();
         Consumer c = new Consumer();
         c.start();
-
     }
 
 }
