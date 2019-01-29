2d2bd44581b564ee7b7bc98fd6402b6e7771a46a
crayonist <462930613@qq.com>
2019-01-29 15:16:34 +0800

    死锁

---
+++ src/main/java/com/labi/thread/deadlock/DeadLock.java
@@ -0,0 +1,42 @@
+package com.labi.thread.deadlock;
+
+public class DeadLock {
+
+    private static final Object resourceA = new Object();
+    private static final Object resourceB = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread threadA = new Thread(() -> {
+            synchronized (resourceA) {
+                System.out.println("threadA get resourceA lock");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (resourceB) {
+                    System.out.println("threadA get resourceB lock");
+                }
+            }
+        });
+
+        Thread threadB = new Thread(() -> {
+            synchronized (resourceB) {
+                System.out.println("threadB get resourceB lock");
+                synchronized (resourceA) {
+                    System.out.println("threadB get resourceA lock");
+                }
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+
+        threadA.join();
+        threadB.join();
+
+        System.out.println("main over");
+    }
+
+}
